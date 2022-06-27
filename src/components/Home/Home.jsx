b0fa38fb6dd1c003e490d6be528004d35a58e05e
GrantRT <granto_92@hotmail.com>
2022-06-27 11:25:22 +0100

    added fixed contact footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './home.css';
 import PHOTO from '../../assets/Images/profile-photo.jpg';
-import { FaGithubSquare } from 'react-icons/fa';
-import { MdAlternateEmail } from 'react-icons/md';
-import { FaLinkedin } from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -14,17 +11,6 @@ const Home = () => {
           <h1>Grant Tolhurst</h1>
           <h2 className="light-colour">Fullstack Web Developer / Software Engineer</h2>
           <img src={PHOTO} className="photo" alt="Grant Tolhurst" />
-          <div className="home__contact">
-            <a href="https://github.com/GrantRT" target="_blank">
-              <FaGithubSquare />
-            </a>
-            <a href="mailto:granto_92@hotmail.com" target="_blank">
-              <MdAlternateEmail />
-            </a>
-            <a href="https://www.linkedin.com/in/grant-tolhurst/" target="_blank">
-              <FaLinkedin />
-            </a>
-          </div>
         </div>
       </header>
     </>
